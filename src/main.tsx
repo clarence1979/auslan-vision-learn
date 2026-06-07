d2885e02b16ed8d8db59c3cef270ccc54520237c
Clarence <47097322+clarence1979@users.noreply.github.com>
2026-06-07 09:53:34 +0000

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
 
-if (window.location.hostname === 'auslan.teachingtools.dev') {
-  window.location.replace('https://teachingtools.dev');
-}
-
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <App />
